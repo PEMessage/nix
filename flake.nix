b1bdef5c4ea9958d5841070484ac09fe5f6a8ff0
PEMessage <1165739182@qq.com>
2026-08-09 21:45:52 +0800

    os/x: add noctalia

--- flake.nix
+++ flake.nix
@@ -12,6 +12,10 @@
       url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    noctalia = {
+      url = "github:noctalia-dev/noctalia";
+      inputs.nixpkgs.follows = "nixpkgs"; # this line is optional, prevents downloading two versions of nixpkgs but disables cache
+    };
   };
 
   # groups: core (have to) / dev / gui (shared by wsl and x) / x (desktop)
--- os/modules/niri.nix
+++ os/modules/niri.nix
@@ -1,11 +1,24 @@
 # niri: scrollable-tiling Wayland compositor (replaces GNOME on desktop hosts).
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, inputs, ... }:
 {
+  imports = [
+    inputs.noctalia.nixosModules.default
+  ];
+
+  programs.noctalia = {
+    enable = true;
+    # Enables NetworkManager, Bluetooth, UPower, and a power profile service.
+    recommendedServices.enable = true;
+  };
+  nix.settings = {
+    extra-substituters = [ "https://noctalia.cachix.org" ];
+    extra-trusted-public-keys = [ "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4=" ];
+  };
+
   programs.niri.enable = true;
   environment.systemPackages = with pkgs; [
     fuzzel
   ];
-
   # Lightweight login manager with autologin straight into niri.
   services.greetd = {
     enable = true;
