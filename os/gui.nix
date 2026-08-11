851c08a98d0a7e5f47de88db7a4d7d8227641546
PEMessage <1165739182@qq.com>
2026-08-12 00:43:58 +0800

    os/gui: update font config

--- os/gui.nix
+++ os/gui.nix
@@ -1,6 +1,34 @@
 { config, pkgs, ... }:
 {
-  fonts.packages = with pkgs; [
-    cascadia-code
-  ];
+  # Thanks to: https://zhuanlan.zhihu.com/p/463403799
+  fonts = {
+    fonts= with pkgs; [
+      adwaita-fonts # GNOME font
+      noto-fonts-color-emoji
+      nerd-fonts.symbols-only
+
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+      cascadia-code
+    ];
+
+    fontconfig = {
+      defaultFonts = {
+        emoji = [ "Noto Color Emoji" ];
+        monospace = [
+          "Noto Sans Mono CJK SC"
+          "DejaVu Sans Mono"
+        ];
+        sansSerif = [
+          "Noto Sans CJK SC"
+          "DejaVu Sans"
+        ];
+        serif = [
+          "Noto Serif CJK SC"
+          "DejaVu Serif"
+        ];
+      };
+    };
+  };
+
 }
