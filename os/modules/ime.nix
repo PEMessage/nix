91b40825315a65b6f1ba9a1186ffde6ae3c487c8
PEMessage <1165739182@qq.com>
2026-08-30 21:22:35 +0800

    ime: enable rime by default

--- os/modules/ime.nix
+++ os/modules/ime.nix
@@ -23,6 +23,18 @@
          })
         librime
       ];
+      settings = {
+        inputMethod = {
+          GroupOrder."0" = "Default";
+          "Groups/0" = {
+            Name = "Default";
+            "Default Layout" = "us";
+            DefaultIM = "keyboard-us";
+          };
+          "Groups/0/Items/0".Name = "keyboard-us";
+          "Groups/0/Items/1".Name = "rime";
+        };
+      };
     };
   };
 
